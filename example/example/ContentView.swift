45bb7d305b7f8b2fd4875481707e7b7c660598a3
Nemo <>
2024-08-20 19:38:16 +0800

    Add a part to print the logic result to the console

--- example/example/ContentView.swift
+++ example/example/ContentView.swift
@@ -8,6 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        let result1 = maxTreasureValue1(value1: 10, weight1: 5, value2: 6, weight2: 4, maxW: 8)
+        let result2 = maxTreasureValue1(value1: 10, weight1: 5, value2: 6, weight2: 4, maxW: 9)
+        let result3 = maxTreasureValue1(value1: 5, weight1: 3, value2: 7, weight2: 4, maxW: 6)
+        
+        let result4 = maxTreasureValue2(values: [10, 6], weights: [5, 4], maxW: 8)
+        let result5 = maxTreasureValue2(values: [10, 6], weights: [5, 4], maxW: 9)
+        let result6 = maxTreasureValue2(values: [5, 7], weights: [3, 4], maxW: 6)
+
+        
+        print("Result 1: \(result1)")
+        print("Result 2: \(result2)")
+        print("Result 3: \(result3)")
+        print("Result 4: \(result4)")
+        print("Result 5: \(result5)")
+        print("Result 6: \(result6)")
+    }
+    
     let tabs = Tab.tabs
     @State private var selectedTab: Int = 0
     @State var isLoading: Bool = true
